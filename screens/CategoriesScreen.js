b1e055329eea40adf8d69c4afb6b8743631f30e5
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-02-19 22:29:07 +0000

    added screen to display all food categories

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -3,13 +3,19 @@ import { FlatList, View } from "react-native";
 import CategoryGridTile from "../components/CategoryGridTile";
 import { CATEGORIES } from "../data/dummy-data";
 
-function renderCategoryItem(itemData) {
-  return (
-    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
-  );
-}
-
-const CategoriesScreen = () => {
+const CategoriesScreen = ({ navigation }) => {
+  function renderCategoryItem(itemData) {
+    function pressHandler() {
+      navigation.navigate("MealsOverview", { categoryId: itemData.item.id });
+    }
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  }
   return (
     <View>
       <FlatList
