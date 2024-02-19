5e91caf34e259544b775c532f0acf82172ca7aab
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-02-19 18:13:05 +0000

    added screen for categories

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { FlatList, View } from "react-native";
+import CategoryGridTile from "../components/CategoryGridTile";
+import { CATEGORIES } from "../data/dummy-data";
+
+function renderCategoryItem(itemData) {
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
+}
+
+const CategoriesScreen = () => {
+  return (
+    <View>
+      <FlatList
+        data={CATEGORIES}
+        renderItem={renderCategoryItem}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
+};
+
+export default CategoriesScreen;
