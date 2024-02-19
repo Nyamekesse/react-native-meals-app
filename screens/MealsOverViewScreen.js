097c56163c5dc393ea084a57768ea716295c37e0
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-02-19 22:22:00 +0000

    added screen to display list of meals

---
+++ screens/MealsOverViewScreen.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import MealItem from "../components/MealItem";
+import { MEALS } from "../data/dummy-data";
+
+const MealsOverViewScreen = ({ route }) => {
+  const catId = route.params.categoryId;
+  const displayedMeal = MEALS.filter(
+    (mealItem) => mealItem.categoryIds.indexOf(catId) >= 0
+  );
+
+  function renderMealItem({ item }) {
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration,
+    };
+    return <MealItem {...mealItemProps} />;
+  }
+  return (
+    <View style={styles.container}>
+      <Text>MealsOverViewScreen - {catId}</Text>
+      <FlatList
+        data={displayedMeal}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
+
+export default MealsOverViewScreen;
