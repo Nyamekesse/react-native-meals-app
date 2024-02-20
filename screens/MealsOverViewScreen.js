90c4c9027e214e2782d84e0e873906faea73c146
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-02-20 10:46:11 +0000

    added dynamic change of screen title

--- screens/MealsOverViewScreen.js
+++ screens/MealsOverViewScreen.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import MealItem from "../components/MealItem";
-import { MEALS } from "../data/dummy-data";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
 
-const MealsOverViewScreen = ({ route }) => {
+const MealsOverViewScreen = ({ route, navigation }) => {
   const catId = route.params.categoryId;
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
+
   const displayedMeal = MEALS.filter(
     (mealItem) => mealItem.categoryIds.indexOf(catId) >= 0
   );
