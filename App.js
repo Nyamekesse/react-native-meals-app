60f8404cbea499b7371dfe517851dc9e4de98c1b
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-02-21 15:49:09 +0000

    wrapped app with Provider from react redux

--- App.js
+++ App.js
@@ -4,14 +4,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
+import { Provider } from 'react-redux';
 import CategoriesScreen from './screens/CategoriesScreen';
 import FavoriteScreen from './screens/FavoriteScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
 import MealsOverViewScreen from './screens/MealsOverViewScreen';
-import {
-  default as FavoriteContextProvider,
-  default as FavoritesContextProvider,
-} from './store/context/favorites-context';
+import FavoritesContextProvider from './store/context/favorites-context';
+import { store } from './store/redux/store';
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -59,7 +58,8 @@ export default function App() {
   return (
     <>
       <StatusBar style='light' />
-      <FavoritesContextProvider>
+      {/* <FavoritesContextProvider> */}
+      <Provider store={store}>
         <NavigationContainer>
           <Stack.Navigator
             screenOptions={{
@@ -90,7 +90,8 @@ export default function App() {
             />
           </Stack.Navigator>
         </NavigationContainer>
-      </FavoritesContextProvider>
+      </Provider>
+      {/* </FavoritesContextProvider> */}
     </>
   );
 }
